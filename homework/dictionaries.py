a2d3bcb897754a09a0d6fd437385acb302976c14
Kayla-Ard <kaylard@yahoo.com>
2024-05-06 21:00:05 -0600

    updates with A LOT of errors

--- homework/dictionaries.py
+++ homework/dictionaries.py
@@ -46,15 +46,15 @@ def open_new_ticket(tickets, counter):
     id_ticket_num_counter = int(3)
     print("Let's get your information for your new ticket!")
     counter = "Ticket00"+str(id_ticket_num_counter)
-    print("Your unique id ticket number has been created! It is " + counter)
+    print("Your unique id ticket number has been created! It is: " + counter)
     
-    name = input("Enter first name to appear on new ticket: ").lower()
-    if name == {name}.isalpha():
+    name = input("Enter first name to appear on new ticket: ").lower().strip()
+    if name.isalpha() == True: 
         print("Thank you! Your name was stored successfully!")
     else:
         print("Please enter a valid name consisting of letters only!")
     
-    issue = input('Enter issue description: Either "login problem", "payment issue", or "lost ticket"').lower()
+    issue = input('Enter issue description: (Either "login problem", "payment issue", or "lost ticket:" ').lower().strip()
     if issue == "login problem" or issue == "payment issue" or issue == "lost ticket":
         print("Thank you! Your issue was stored successfully!")
     else:
@@ -65,13 +65,62 @@ def open_new_ticket(tickets, counter):
         print("Thank you! Your status was stored successfully!")
     else:
         print("Please enter a valid response!")
-        
+    
+    new_ticket = {{counter}: {"Customer": {name}, "Issue": {issue}, "Status": {status}}}
+    tickets.update(new_ticket)  
 
 def update_status(tickets):
-    pass
+    print("Let's get your status changed!")
+    status = input("Enter you unique id ticket number you would like to change the status on: ").lower()
+    for status in tickets:
+        if status in tickets:
+            if tickets[{status}]["Status"] == "open":
+                tickets[{status}]["Status"] = "closed"
+            elif tickets[{status}]["Status"] == "closed":
+                tickets[{status}]["Status"] = "open"
+        else:
+            print("That unique id ticket number does not exist")
+            
+def display_tickets(tickets, counter):
+    display = input("""Choose an option from the menu below on which tickets you would like to view: 
+    Menu:
+        "1" = View all active tickets 
+        "2" = View all tickets with a login problem issue
+        "3" = View all tickets with a payment issue
+        "4" = View all tickets with lost ticket issue 
+        "5" = View all tickets with an open status
+        "6" = View all tickets with a closed status 
+        "7" = Return to Main Menu
+        """).lower()
+    
+    if display == "1":
+        print(tickets)
         
-def display_tickets(tickets):
-    pass
+    elif display == "2":
+        for key in tickets:
+            print(tickets[counter]["Issue"] == ["login problem"])
+    
+    elif display == "3":
+        for key in tickets:
+            print(tickets[counter]["Issue"] == ["payment issue"])
+    
+    elif display == "4":
+        for key in tickets:
+            print(tickets[counter]["Issue"] == ["lost ticket"])
+    
+    elif display == "5":
+        for key in tickets:
+            print(tickets[counter]["Status"] == ["open"])
+    
+    elif display == "6":
+        for key in tickets:
+            print(tickets[counter]["Status"] == ["closed"])
+    
+    elif display == "7":
+        print("Returning to Main Menu")
+        
+    else:
+        print("You entered an invalid option!")
 
 def track_service_tickets(tickets, counter):
     id_ticket_num_counter = int(2)
@@ -85,18 +134,18 @@ def track_service_tickets(tickets, counter):
             "update" = To update the status of an existing ticket 
             "display" = To display all tickets or filter by status           
             "done" = To exit the program    
-            """)
+            """).lower()
         
         if action == "open":
-            id_ticket_num_counter += int(1)
             open_new_ticket(tickets, counter)
-            print(service_tickets = {counter : {"Customer": {name}, "Issue": {issue}, "Status": {status}}})        
+            id_ticket_num_counter += int(1)
+            print(tickets)
             
         elif action == "update":
             update_status(tickets)
         
         elif action == "display":
-            display_tickets(tickets)
+            display_tickets(tickets, counter)
             break
         else:
             print("Thanks for using the service tickets app. Good-bye!")
